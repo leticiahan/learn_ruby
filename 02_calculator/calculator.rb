7bf52eab4b67ba24dc83750f7aef05b1f616045c
Leticia Han <lbhan94@gmail.com>
2017-08-24 16:43:27 -0700

    create bonus functions: multiply, power, factorial

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -13,3 +13,27 @@ def sum(array)
   end
   return total
 end
+
+def multiply(array)
+  total = 1
+  array.each do |int|
+    total = total * int
+  end
+  return total
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(a)
+  product = 1
+  if a == 0
+    return product
+  else
+    for i in 1..a
+      product = product * i
+    end
+  end
+  return product
+end
