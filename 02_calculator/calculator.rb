7110f379db4f746f2dcf4b23f33f254561916dce
Leticia Han <lbhan94@gmail.com>
2017-08-24 16:20:57 -0700

    create add, subtract, and sum functions

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  total = 0
+  array.each do |int|
+    total = total + int
+  end
+  return total
+end
